099ca4c1502c20ea8a32d275066540d41bb75528
HUA428571 <hua428571@126.com>
2021-06-23 01:03:03 +0800

    碰撞函数重写已经完成，解决了棋子穿过去的问题，外加了失败掉下去的动画。（这是一个保护性提交，为了防止文件丢失）--HuaCL

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
 import javax.swing.*;
 
 public class Main {
+    private static double[] minTime = new double[]{30.00,25.00,20.00};//关卡难度最短时间
+    public static double getMinTime(int level)
+    {
+        return minTime[level-1];
+    }
+    public static void setMinTime(int level,double time)
+    {
+        minTime[level-1]=time;
+    }
     public static ImageIcon scaleImage(ImageIcon icon, int w, int h) //该函数用于按照比例拉伸ImageIcon
     {
         int nw = icon.getIconWidth();
@@ -22,11 +34,46 @@ public class Main {
         return icon;
     }
     public static void main(String[] args) {
-
         //创建窗口
-        Level frame = new Level(1);
-//        System.out.println("我返回了");
-//        Level frame2 = new Level(1);
+        JFrame homePage=new JFrame();
+        homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        homePage.setVisible(true);
+        homePage.setResizable(false);//固定大小
+        homePage.setBounds(350, 300,400,400);
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(250, 250, 250));
+        panel.setLayout(null);
+        panel.setBounds(0,0,400,400);
+        homePage.add(panel);
+        JLabel Welcome=new JLabel();
+        Welcome.setBounds(70,70,300,40);
+        Welcome.setFont(new Font("Times New Roman",Font.BOLD,20));
+        Welcome.setForeground(Color.black);
+        Welcome.setVisible(true);
+        panel.add(Welcome);
+        Welcome.setText("Welcome To Play JumpJump!!!");
+        Welcome.setForeground(Color.black);
+        JButton play1P=new JButton("Play1P");
+        JButton play2P=new JButton("Play2P");
+        play1P.setBounds(130,140,140,30);
+        play2P.setBounds(130,200,140,30);
+        panel.add(play1P);
+        panel.add(play2P);
+        //WindowListener
+        play1P.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                homePage.setVisible(false);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Level frame = new Level(1);
+                    }
+                }).start();
+
+            }
+        });
 
     }
 }
--- src/com/company/TimeCounter.java
+++ src/com/company/TimeCounter.java
@@ -8,13 +8,12 @@ public class TimeCounter extends JLabel
     private double[] limitTime = new double[]{30.00,25.00,20.00};//关卡难度时间
     private double gameTime;//单局游戏时间
     //当前全部关卡，从1开始
-    private int level = 0;
     public TimeCounter(Level level)
     {
         setBounds(800,0,200,40);
         double limitTime=getLimitTime(1);
         double gameTime=0;
-        double minTime=level.getMinTime(level.getLevel());
+        double minTime=Main.getMinTime(level.getLevel());
         setFont(new Font("Times New Roman",Font.BOLD,15));
         setText("<html>Count Down："+String.format("%.2f", limitTime-gameTime)+"秒<br/>Fastest Record："+String.format("%.2f", minTime)+"秒</html>");
         setForeground(Color.black);
@@ -46,7 +45,7 @@ public class TimeCounter extends JLabel
     public void timeChange (Level level)
     {
         double limitTime=getLimitTime(1);
-        double minTime=level.getMinTime(level.getLevel());
+        double minTime=Main.getMinTime(level.getLevel());
         setFont(new Font("Times New Roman",Font.BOLD,15));
         setText("<html>Count Down："+String.format("%.2f", (limitTime-gameTime))+"秒<br/>Fastest Record："+String.format("%.2f", minTime)+"秒</html>");
         setForeground(Color.black);
